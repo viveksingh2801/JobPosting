791dd13f4ab70017240d6f7f98d9271a424bbc7d
Vivek Singh <vivek280197@gmail.com>
2025-02-23 23:10:12 +0530

    last commit

--- backend/server.js
+++ backend/server.js
@@ -10,20 +10,20 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middleware;
-// app.use(
-//   cors({
-//     origin: "https://jobposting-frontend-t4io.onrender.com",
-//     credentials: true,
-//   })
-// );
-
+Middleware;
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://jobposting-frontend-t4io.onrender.com",
     credentials: true,
   })
 );
+
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
 
 app.use(express.json());
 app.use(cookieParser());
