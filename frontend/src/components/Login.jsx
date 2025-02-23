b7575c0f77e214c27354d22c457fe0db1593790d
Vivek Singh <vivek280197@gmail.com>
2025-02-23 23:23:22 +0530

    last commit

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -6,8 +6,8 @@ import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
-  const [email, setEmail] = useState("vicky@gmail.com");
-  const [password, setPassword] = useState("28@Vicky");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
